01552009f7925343024ff4ebc3e483adc527f346
momororororo <morozumidaiki@yahoo.co.jp>
2023-02-12 17:13:45 +0900

    init

---
+++ src/urdf_test/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'urdf_test'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+
+        (os.path.join('share',package_name,'launch'),glob('launch/*.py')),
+        (os.path.join('share',package_name,'urdf'),glob('urdf/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='morozumi',
+    maintainer_email='daiki_morozumi@jp.honda',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
